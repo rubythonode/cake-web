5cdf42576bbf040ab015b4405f1b74d9ebbc45c1
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-08 13:59:42 +0900

    feat: show steps in apply page

--- packages/frontend/src/pages/Apply.tsx
+++ packages/frontend/src/pages/Apply.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Switch, { Case } from 'react-switch-case';
 import styled from 'styled-components';
 
 import Layout from '../components/Layout';
@@ -15,22 +16,54 @@ type ApplyProps = {
 };
 
 type ApplyState = {
-
+  location: string;
+  step: number;
 };
 
 export default class Apply extends React.Component<ApplyProps, ApplyState> {
   constructor(props: ApplyProps) {
     super(props);
+
+    this.state = {
+      location: '',
+      step: 0,
+    };
+  }
+
+  public onClickNext() {
+    this.setState(prevState => ({
+      step: prevState.step + 1,
+    }));
+  }
+
+  public onClickCard(location: string) {
+    this.setState({
+      location,
+    });
+    this.onClickNext();
   }
 
   public render() {
+    const { step } = this.state;
     return (
       <Layout tabIdx={1}>
-        <Title>공간 선택</Title>
-        <PlaceCardList>
-          <PlaceCard name="본관" image="main.png" />
-          <PlaceCard name="신관" image="new.png" />
-        </PlaceCardList>
+        <Switch condition={step}>
+          <Case value={0}>
+            <Title>공간 선택</Title>
+            <PlaceCardList>
+              <PlaceCard
+                name="본관"
+                onClick={() => this.onClickCard('본관')}
+                image="main.png"
+              />
+              <PlaceCard
+                name="신관"
+                onClick={() => this.onClickCard('신관')}
+                image="new.png"
+              />
+            </PlaceCardList>
+          </Case>
+        </Switch>
       </Layout>
     );
   }
