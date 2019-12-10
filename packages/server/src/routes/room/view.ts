3db81520d0bfa5e4266138a2d51892126608bd13
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-10 20:06:23 +0900

    feat: room list endpoint

--- packages/server/src/routes/room/view.ts
+++ packages/server/src/routes/room/view.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
 import expressAsyncHandler from 'express-async-handler';
+import roomModel, { IRoomModel } from '../../models/roomModel';
 
 const router: express.IRouter = Router();
 
 router.get('/', expressAsyncHandler(async (req, res, _) => {
-  return res.json({});
+  const rooms: IRoomModel[] = await roomModel.find({});
+  return res.json({ rooms });
 }));
 
 export default router;
