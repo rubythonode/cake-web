8ed31e6ae06559b8d766928d68a564c4021bb3fc
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-10 17:42:08 +0900

    chore: update dependencies

--- packages/server/src/routes/room/view.ts
+++ packages/server/src/routes/room/view.ts
@@ -1,5 +1,10 @@
 import express, { Router } from 'express';
+import expressAsyncHandler from 'express-async-handler';
 
 const router: express.IRouter = Router();
+
+router.get('/', expressAsyncHandler(async (req, res, _) => {
+  return res.json({});
+}));
 
 export default router;
